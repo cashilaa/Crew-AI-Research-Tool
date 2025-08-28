8489466cabfa8c1e807e3c71e9954d417c43de75
Your Name <you@example.com>
2025-08-28 18:43:23 +0300

    fixed

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 # Fix SQLite compatibility issue for ChromaDB
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
+
 import os
 os.environ["ALLOW_RESET"] = "TRUE"
 
